7af127ca09726230fd97e4717ae625bf48a66f37
Heloise Taillet <ht2394@columbia.edu>
2018-12-26 18:11:00 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,31 @@
 # Write your code here.
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  narray = []
+  array.each do |e|
+    string = badge_maker(e)
+    narray << string
+  end
+  narray
+end
+
+def assign_rooms(array)
+  narray = []
+  array.each_with_index do |e, i|
+    narray << "Hello, #{e}! You'll be assigned to room #{i+1}!"
+  end
+    narray
+end
+
+def printer(array)
+  i=0
+  while i < array.size
+    puts batch_badge_creator(array)[i]
+    puts assign_rooms(array)[i]
+    i+=1
+  end
+end
