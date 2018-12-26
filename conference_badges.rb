a570e0291703181131dd88ba805280df762c1f65
Heloise Taillet <ht2394@columbia.edu>
2018-12-26 17:52:19 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,24 @@
 # Write your code here.
+
+def badge_maker(name)
+  puts "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  narray = []
+  array.each do |e|
+    string = badge_maker(e)
+    narray << string
+  end
+  narray
+end
+
+def assign_rooms(array)
+  array.each_with_index do |e, i|
+  puts "Hello, #{e}! You'll be assigned to room #{i+1}!"
+end
+end
+
+def printer(array)
+
+end
