98814666384ba61b9a09e9c87c998dc06f7fb137
Heloise Taillet <ht2394@columbia.edu>
2018-12-26 18:10:51 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -16,16 +16,16 @@ end
 def assign_rooms(array)
   narray = []
   array.each_with_index do |e, i|
-  narray << "Hello, #{e}! You'll be assigned to room #{i+1}!"
-end
-narray
+    narray << "Hello, #{e}! You'll be assigned to room #{i+1}!"
+  end
+    narray
 end
 
 def printer(array)
   i=0
   while i < array.size
-puts batch_badge_creator(array)[i]
-puts assign_rooms(array)[i]
-i+=1
-end
+    puts batch_badge_creator(array)[i]
+    puts assign_rooms(array)[i]
+    i+=1
+  end
 end
